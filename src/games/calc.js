a0d20e04da93b631e7e8b9a2ece3eec5801207d7
Bakyt Abdesh <130103041.stu.sdu.edu@gmail.com>
2024-08-14 23:01:24 +0500

    feat: add calc.js

---
+++ src/games/calc.js
@@ -0,0 +1,44 @@
+import readlineSync from 'readline-sync';
+import {
+  cons, car, cdr,
+} from '@hexlet/pairs';
+import { greeting, congrats, fail } from '../index.js';
+import randomNum from '../getRandonNum.js';
+
+export default () => {
+  const operators = ['+', '-', '*'];
+  const question = 'What is the result of the expression?';
+  greeting(question);
+  let correctAnswers = 0;
+  for (correctAnswers; correctAnswers < 3; correctAnswers += 1) {
+    const nums = cons(randomNum(1, 100), randomNum(1, 100));
+    const a = car(nums);
+    const b = cdr(nums);
+    const randomOperator = Math.floor(Math.random() * operators.length);
+    const operator = operators[randomOperator];
+    console.log(`Question: ${a} ${operator} ${b}`);
+    const yourAnswer = readlineSync.question('Your answer: ');
+    let correctAnswer = 0;
+    switch (operator) {
+      case '+':
+        correctAnswer = a + b;
+        break;
+      case '-':
+        correctAnswer = a - b;
+        break;
+      case '*':
+        correctAnswer = a * b;
+        break;
+      default:
+        break;
+    }
+    if (parseInt(yourAnswer, 10) === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${yourAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      fail();
+      break;
+    }
+  }
+  congrats(correctAnswers);
+};
